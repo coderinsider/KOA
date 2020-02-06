514914b01f90671bb3622ad16addc8561184dc47
Myat Ko Ko <coderinsider1@gmail.com>
2020-02-06 17:55:41 +0630

    Setup for async use

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const httpPort = 23435;
 // add a date method to the context
 app.context.studentNams = ["Maung Maung", "Aung Aung", "Aye Aye"];
 app.context.date = new Date().getFullYear();
-app.context.useraData = {
+app.context.userData = {
     'first' : 'Myat',
     'last'  : 'Ko Ko',
     'programming' : 'HTML, CSS, JavaScript, jQuery, Bootstrap, PHP, MYSQL, Laravel, Node.js, Express.js, Kos.js',
@@ -12,20 +12,27 @@ app.context.useraData = {
     'township' : 'Tarmwe'
 }
 // Response
-app.use(ctx => {
+app.use(async (ctx) => {
+    try {
+        return ctx.response.body = await  ctx.userData;
+    } catch (error) {
+        console.log(error);
+    }
     ctx.state.user = "Hello, How are you feeling today " ;
     // Request object usage
     let from = ctx.request.method;
     console.log(from);
     console.log(ctx.userData);
-    if(ctx.userData) { // Is user data true
-        return ctx.response.body = ctx.userData;
-    } else {
-        return ctx.throw(400, ' data required');
-    }
+    // if(ctx.userData) { // Is user data true
+    //     return ctx.response.body = ctx.userData;
+    // } else {
+    //     return ctx.throw(400, ' data required');
+    // }
     // Print out name with date
    // ctx.body = ctx.state.user + " " + ctx.studentNams[0] + " " + ctx.date;
 });
+
+
 app.listen(httpPort);
 console.log("Server Running follower ip: " + httpPort);
 console.log(app.subdomainOffset);
